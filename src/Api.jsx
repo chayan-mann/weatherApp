416c65cadd1769a9abc68d39c2652f788d2258dd
blood <chayanmann09@gmail.com>
2024-05-10 13:57:31 +0530

    second commit

---
+++ src/Api.jsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": '666a50c72cmsh6e1befdadcbeeefp1fc633jsn86aba38658cc',// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "a6055c6d5ffe0ac9c29f1c89655de923"; 
+  // enter your key from openweather API
